dda66f453b78d71fdbb350ece6ee6e3085fb619b
andrewlyden <36414677+andrewlyden@users.noreply.github.com>
2023-11-09 16:13:29 +0000

    edit

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP]
 server = app.server
 
 app.layout = html.Div([
-    html.H1('Multi-page app with Dash Pages'),
-    html.Div([
-        html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
-        ) for page in dash.page_registry.values()
-    ]),
+    # html.H1('Multi-page app with Dash Pages'),
+    # html.Div([
+    #     html.Div(
+    #         dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+    #     ) for page in dash.page_registry.values()
+    # ]),
     dash.page_container
 ])
 
